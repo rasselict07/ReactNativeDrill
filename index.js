1f6b8f48a20278516bf463bde7fa36e82c96aea3
rasselict07 <rezaul07012>
2018-12-26 23:50:59 +0600

    Add header and multiple component in main component

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/** @format */
+// Import a library to help create a component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a component
+const App = () => (
+    <View>
+        <Header headerText={'Albums Header!!'} />
+        <AlbumList />
+    </View>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+// Render it to the device
+AppRegistry.registerComponent('ReactNativeDrill', () => App);
